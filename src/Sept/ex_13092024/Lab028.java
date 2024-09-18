8ab7efd77ca6417afe96c6ada160942ad995d01f
Digambar_Jadhav <jadhavdigambar5371@gmail.com>
2024-09-18 22:24:44 +0530

    Test

---
+++ src/Sept/ex_13092024/Lab028.java
@@ -0,0 +1,9 @@
+package Sept.ex_13092024;
+
+public class Lab028 {
+    public static void main(String[] args) {
+        int a = 10;
+        float PIE = 3.14F;
+        System.out.println(a+PIE);
+    }
+}
---
+++ src/Sept/ex_16092024/Lab30_Assignment_Operator.java
@@ -0,0 +1,9 @@
+package Sept.ex_16092024;
+
+public class Lab30_Assignment_Operator {
+    public static void main(String[] args) {
+        int a = 45;
+        // = Assignment Operator
+        //right hand side value to variable
+    }
+}
