1bff27a5fc096e866792c8fa10abf317ff93e983
Digambar_Jadhav <jadhavdigambar5371@gmail.com>
2024-09-18 22:12:15 +0530

    Test

---
+++ src/Sept/ex_13092024/Lab026.java
@@ -0,0 +1,13 @@
+package Sept.ex_13092024;
+public class Lab026 {
+    public static void main(String[] args) {
+        // Constant
+        // constants are variables
+        // whose values cannot be changed once they are assigned.
+        // final - final dataType CONSTANT_NAME = value;
+        final float PIE = 3.1456f;
+        //PIE = 12.34f; => Gives error as value of PIE is final by Final Keyword
+        System.out.println(PIE);
+
+    }
+}
