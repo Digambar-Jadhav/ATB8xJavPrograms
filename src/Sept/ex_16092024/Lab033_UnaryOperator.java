8208aca77bfd6867ba4658beecf82c7877f213a3
Digambar_Jadhav <jadhavdigambar5371@gmail.com>
2024-09-18 22:32:51 +0530

    Test

---
+++ src/Sept/ex_16092024/Lab033_UnaryOperator.java
@@ -0,0 +1,13 @@
+package Sept.ex_16092024;
+
+public class Lab033_UnaryOperator {
+    public static void main(String[] args) {
+        int a = +65; // + ->  unary operator
+        int b = -1; //- -> Unary Operator
+        System.out.println(a);
+        System.out.println(b);
+        b = b+1;
+        System.out.println(b);
+
+    }
+}
