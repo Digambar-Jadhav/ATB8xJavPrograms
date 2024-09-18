ba08b8f48cde78e9f66eba3399bb98df0d2aba54
Digambar_Jadhav <jadhavdigambar5371@gmail.com>
2024-09-18 23:02:07 +0530

    Test

---
+++ src/Sept/ex_16092024/Lab034.java
@@ -0,0 +1,12 @@
+package Sept.ex_16092024;
+
+public class Lab034 {
+    public static void main(String[] args) {
+        String first_name = "Digambar";
+        String last_name = "Jadhav";
+        // Concatenation Operator  +
+        // + -> int + int -> math
+        // + -> String -> combine -> concat
+        System.out.println(first_name + last_name);
+    }
+}
---
+++ src/Sept/ex_16092024/Lab035.java
@@ -0,0 +1,13 @@
+package Sept.ex_16092024;
+
+public class Lab035 {
+    public static void main(String[] args) {
+        String first_name = "Digambar";
+        String last_name = "Jadhav";
+        int a = 10;
+        int b = 10;
+        System.out.println(first_name + last_name +a + b);
+        System.out.println(first_name + last_name +(a + b));
+        System.out.println(a + b + first_name + last_name);
+    }
+}
