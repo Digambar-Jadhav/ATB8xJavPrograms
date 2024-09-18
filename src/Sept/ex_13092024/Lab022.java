b3f11de26098b52dd48a5d1a1260d367f8c60923
Digambar_Jadhav <jadhavdigambar5371@gmail.com>
2024-09-18 21:56:24 +0530

    Test

---
+++ src/Sept/ex_13092024/Lab022.java
@@ -0,0 +1,9 @@
+package Sept.ex_13092024;
+
+public class Lab022 {
+    public static void main(String[] args) {
+        String name = "Digambar";
+        String last_name = "Jadhav";
+        System.out.printf("Your first name is %s and last name is %s ",name, last_name);
+    }
+}
