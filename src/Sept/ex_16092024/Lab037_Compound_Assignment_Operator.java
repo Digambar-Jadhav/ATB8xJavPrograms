bab9cf65d3f5649d3cf66d6a668ba30424c59e9f
Digambar_Jadhav <jadhavdigambar5371@gmail.com>
2024-09-18 23:17:35 +0530

    Test

---
+++ src/Sept/ex_16092024/Lab037_Compound_Assignment_Operator.java
@@ -0,0 +1,21 @@
+package Sept.ex_16092024;
+
+public class Lab037_Compound_Assignment_Operator {
+    public static void main(String[] args) {
+        //  Compound Assignment Operators
+        int a = 10;
+        // += ,-= , *=, /=
+        a += 20; // a = a+ 20
+        System.out.println(a);
+
+        a -= 20; // a = a - 20
+        System.out.println(a);
+
+        a /= 10; // a = a/10
+        System.out.println(a);
+
+        a*=10; // a = a*10
+        System.out.println(a);
+
+    }
+}
