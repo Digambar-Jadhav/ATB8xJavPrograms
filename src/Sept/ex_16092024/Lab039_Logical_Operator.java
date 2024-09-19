ce0376f17fe26cd2052878019c5fcf759e874d94
Digambar_Jadhav <jadhavdigambar5371@gmail.com>
2024-09-19 08:29:39 +0530

    Test

---
+++ src/Sept/ex_16092024/Lab039_Logical_Operator.java
@@ -0,0 +1,14 @@
+package Sept.ex_16092024;
+
+public class Lab039_Logical_Operator {
+    public static void main(String[] args) {
+        boolean b = true;
+        boolean c = true;
+        //boolean a = true && true;
+        boolean a = true || false;
+        System.out.println(a);
+        System.out.println(!b);
+        System.out.println(!c);
+
+    }
+}
