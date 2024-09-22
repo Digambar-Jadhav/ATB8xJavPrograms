99186d687da4f25204d94cb7914627b25b8e851c
Digambar_Jadhav <jadhavdigambar5371@gmail.com>
2024-09-22 17:27:59 +0530

    Test

---
+++ src/Task013092024/Task002.java
@@ -0,0 +1,14 @@
+package Task013092024;
+
+public class Task002 {
+    public static void main(String[] args) {
+        //Task2----calculator
+        double a=34;
+        double b=10;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+
+    }
+}
---
+++ src/Task013092024/Task003.java
@@ -0,0 +1,11 @@
+package Task013092024;
+
+public class Task003 {
+    public static void main(String[] args) {
+        byte b =10; // 1 Byte
+        long l=10l; // 8 Byte
+
+        long result= b+l;
+        System.out.println(result);    // total = 9 Byte and 72 bits
+    }
+}
