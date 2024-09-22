ae77b753f7b7cea44633539dadad31e4fa414e9b
Digambar_Jadhav <jadhavdigambar5371@gmail.com>
2024-09-22 16:19:43 +0530

    Test

---
+++ src/Sept/ex_20292024/Lab065.java
@@ -0,0 +1,14 @@
+package Sept.ex_20292024;
+
+public class Lab065 {
+    public static void main(String[] args) {
+        // Max, Min between two numbers.
+        int Digambar_Salary = 7;
+        int Anita_Salary = 4;
+
+        int max = Digambar_Salary > Anita_Salary ? Digambar_Salary : Anita_Salary;
+        System.out.println(max);
+        int min = Digambar_Salary < Anita_Salary ? Digambar_Salary : Anita_Salary;
+        System.out.println(min);
+    }
+}
