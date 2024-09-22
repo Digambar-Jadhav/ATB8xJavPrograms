72db6439a024d04f88a0e66360c44481d3c102eb
Digambar_Jadhav <jadhavdigambar5371@gmail.com>
2024-09-22 16:41:24 +0530

    Test

---
+++ src/Sept/ex_20292024/Lab067.java
@@ -0,0 +1,12 @@
+package Sept.ex_20292024;
+import java.lang.Math;
+public class Lab067 {
+    public static void main(String[] args) {
+        int a = 23;
+        int b = 10;
+        System.out.println(Math.max(a,b));
+        System.out.println(Math.min(a,b));
+        System.out.println(Math.sin(90));
+        System.out.println(Math.abs(-1));
+    }
+}
