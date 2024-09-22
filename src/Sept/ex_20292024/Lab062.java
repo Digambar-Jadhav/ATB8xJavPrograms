fcfee075a195cd9b6f94f6c9b15609a52f0ceba0
Digambar_Jadhav <jadhavdigambar5371@gmail.com>
2024-09-22 16:11:24 +0530

    Test

---
+++ src/Sept/ex_20292024/Lab062.java
@@ -0,0 +1,10 @@
+package Sept.ex_20292024;
+
+public class Lab062 {
+    public static void main(String[] args) {
+        // condition ? expression_if_true : expression_if_false
+        int a1 = (30 > 40) ? 10: 20;
+        System.out.println(a1);
+
+    }
+}
