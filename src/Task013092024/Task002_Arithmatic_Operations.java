b7f92476c987e80cd35c60ef8fe31d648d8090ce
Digambar_Jadhav <jadhavdigambar5371@gmail.com>
2024-09-22 17:32:35 +0530

    Test

--- src/Task013092024/Task002_Arithmatic_Operations.java
+++ src/Task013092024/Task002_Arithmatic_Operations.java
@@ -1,8 +1,7 @@
 package Task013092024;
 
-public class Task002 {
+public class Task002_Arithmatic_Operations {
     public static void main(String[] args) {
-        //Task2----calculator
         double a=34;
         double b=10;
         System.out.println(a+b);
