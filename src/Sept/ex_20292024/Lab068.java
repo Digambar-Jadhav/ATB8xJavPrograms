70c5b099fd1a41621ee6dd3c93b06ef5c367c925
Digambar_Jadhav <jadhavdigambar5371@gmail.com>
2024-09-22 17:05:53 +0530

    Test

---
+++ src/Sept/ex_20292024/Lab068.java
@@ -0,0 +1,25 @@
+package Sept.ex_20292024;
+
+public class Lab068 {
+    public static void main(String[] args) {
+        // Conditions
+        // Condition - outputs
+        //
+        // If - Else - condition
+
+        //        if (condition ==> true or false){
+        //
+        //        }else{
+        //
+        //        }
+
+
+        int age = 18;
+        if (age >= 18){
+            System.out.println("You are allowed to Vote");
+        }else {
+            System.out.println("You are not allowed to Vote");
+        }
+
+    }
+}
