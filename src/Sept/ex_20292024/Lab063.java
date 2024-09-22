67d0c3d6ffb7087b335ecdc969fc76a61fc8d351
Digambar_Jadhav <jadhavdigambar5371@gmail.com>
2024-09-22 16:14:23 +0530

    Test

---
+++ src/Sept/ex_20292024/Lab063.java
@@ -0,0 +1,12 @@
+package Sept.ex_20292024;
+
+public class Lab063 {
+    public static void main(String[] args) {
+
+        // condition ? expression_if_true : expression_if_false
+        int Digambar = 4;
+        int Anita = 7;
+        String result = Digambar > Anita ? "Digambar won" : "Anita Won";
+        System.out.println(result);
+    }
+}
