183a46cd7f41e35f7bc9e20ee33542fd33709329
Digambar_Jadhav <jadhavdigambar5371@gmail.com>
2024-09-29 08:27:12 +0530

    Test

--- src/Sept/ex_23092024/Lab078.java
+++ src/Sept/ex_23092024/Lab078.java
@@ -2,6 +2,27 @@ package Sept.ex_23092024;
 
 public class Lab078 {
     public static void main(String[] args) {
+        int day = 5;
+        switch (day){
+            case 1:
+                System.out.println("Mon");
+            case 2:
+                System.out.println("Tue");
+            case 3:
+                System.out.println("Wed");
+            case 4:
+                System.out.println("Thu");
+            case 5:
+                System.out.println("Fri");
+            case 6:
+                System.out.println("Sat");
+            case 7:
+                System.out.println("Sun");
+            default:
+                System.out.println("No idea, Whats day is");
+                break;
 
+        }
+        System.out.println("End of the loop");
     }
-}
+    }
