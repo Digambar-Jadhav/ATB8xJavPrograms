1914622528b2ee063b912e720eeb1324c565f497
Digambar_Jadhav <jadhavdigambar5371@gmail.com>
2024-09-29 21:05:53 +0530

    Test

---
+++ src/Sept/ex_25092024/Lab114.java
@@ -0,0 +1,15 @@
+package Sept.ex_25092024;
+
+import java.util.Scanner;
+
+public class Lab114 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in); //New Object
+        System.out.println("Enter the number");
+        int num = sc.nextInt();
+        System.out.println(num);
+        System.out.println("Enter the String");
+        String name= sc.nextLine();
+        System.out.println(name);
+    }
+}
