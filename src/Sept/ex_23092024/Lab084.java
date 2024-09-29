1a8db52c2c4e478944f187941b2c525f391222f2
Digambar_Jadhav <jadhavdigambar5371@gmail.com>
2024-09-29 14:37:55 +0530

    Test

---
+++ src/Sept/ex_23092024/Lab084.java
@@ -0,0 +1,7 @@
+package Sept.ex_23092024;
+
+public class Lab084 {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ src/Sept/ex_23092024/Lab085.java
@@ -0,0 +1,14 @@
+package Sept.ex_23092024;
+
+public class Lab085 {
+    public static void main(String[] args) {
+        int itemcode = 003;
+        switch (itemcode){
+           case 001-> System.out.println("It's a Laptop");
+           case 002-> System.out.println("It's a Desktop");
+           case 003-> System.out.println("It's a Mobile Phone");
+            default -> System.out.println(" Ha ha ha It's a Pager");
+        }
+
+    }
+}
