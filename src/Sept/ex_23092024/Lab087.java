0c80e74769744457617f5978560ec4458b8eb2b1
Digambar_Jadhav <jadhavdigambar5371@gmail.com>
2024-09-29 15:19:31 +0530

    Test

--- src/Sept/ex_23092024/Lab087.java
+++ src/Sept/ex_23092024/Lab087.java
@@ -1,4 +1,18 @@
 package Sept.ex_23092024;
 
 public class Lab087 {
+    public static void main(String[] args) {
+        char code = 'B';
+        switch (code) {
+            default:
+                System.out.println("Hellooooooo");
+                break;
+            case 'A':
+                System.out.println("65");
+                break;
+            case 'B':
+                System.out.println("66");
+                break;
+        }
+    }
 }
---
+++ src/Sept/ex_23092024/Lab088.java
@@ -0,0 +1,18 @@
+package Sept.ex_23092024;
+
+import javax.xml.transform.Source;
+import java.sql.SQLOutput;
+
+public class Lab088 {
+    public static void main(String[] args) {
+       // char c = 'a'; -> Vovel or COnsonant
+        // vovel-> a, e, o, i, u
+        char ch = 'a';
+        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+            System.out.println(" Vovel");
+        }else{
+                System.out.println("Consonant");
+
+        }
+    }
+}
