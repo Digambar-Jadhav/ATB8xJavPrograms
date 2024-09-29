8c6f87cc4f3bfa5e2a998d6bf64dee031b3a237c
Digambar_Jadhav <jadhavdigambar5371@gmail.com>
2024-09-29 20:52:11 +0530

    Test

---
+++ src/Sept/ex_25092024/Lab110.java
@@ -0,0 +1,20 @@
+package Sept.ex_25092024;
+
+public class Lab110 {
+    public static void main(String[] args) {
+        //while loop
+        //while ->A,B, C
+        //A-Initialization
+        //B-Condition
+        //C-Increament/Decreament
+        int i = 0; //A
+        while (i <= 10){ //B
+            System.out.println(i);
+            i++; //C
+          }
+        for (int j = 0; j <=10 ; j++) {
+            System.out.println(j);
+        }
+
+    }
+}
---
+++ src/Sept/ex_25092024/Lab111.java
@@ -0,0 +1,11 @@
+package Sept.ex_25092024;
+
+public class Lab111 {
+    public static void main(String[] args) {
+        int RaGa = 0;
+        while(RaGa <= 15){
+            System.out.println("Bharat Jodo Yatra");
+            RaGa++;
+        }
+    }
+}
