4dc7600b85c39020712b6ef22fd48a805cec9062
Digambar_Jadhav <jadhavdigambar5371@gmail.com>
2024-09-29 08:35:36 +0530

    Test

---
+++ src/Sept/ex_23092024/Lab079.java
@@ -0,0 +1,28 @@
+package Sept.ex_23092024;
+
+public class Lab079 {
+    public static void main(String[] args) {
+        // Web Automation
+        // I will ask user which browser you want me to run the code.
+        // chrome -> execute chrome
+        // firefox -> execute of firefox
+        // edge -> edge cases.
+        String Browser =  "Chrome";
+switch (Browser){
+        default :
+        System.out.println("No idea what the browser is");
+        break;
+        case "Chrome":
+        System.out.println("This is Chrome Browser");
+        break;
+    case "Edge":
+        System.out.println("This is Edge Browser");
+        break;
+    case "Firefox":
+        System.out.println("This is Firefox Browser");
+        break;
+
+}
+
+    }
+}
