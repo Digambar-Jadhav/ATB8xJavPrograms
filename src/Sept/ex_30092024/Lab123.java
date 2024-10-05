543da908667d2a7c64af3a15819f956aa0cf99cb
Digambar_Jadhav <jadhavdigambar5371@gmail.com>
2024-10-05 13:33:55 +0530

    Test

---
+++ src/Sept/ex_30092024/Lab123.java
@@ -0,0 +1,13 @@
+package Sept.ex_30092024;
+
+
+public class Lab123 {
+    public static void main(String[] args) {
+        function_type5();
+
+
+    }
+    public static void function_type5(){
+        System.out.println("This is just a function testing with void values");
+    }
+}
---
+++ src/Sept/ex_30092024/Lab124_Person.java
@@ -0,0 +1,7 @@
+package Sept.ex_30092024;
+
+public class Lab124_Person {
+    public static void main(String[] args) {
+
+    }
+}
