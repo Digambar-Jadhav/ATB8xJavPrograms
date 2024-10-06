3aa355dd175db6f00eb20751ce2082bdfa549ac2
Digambar_Jadhav <jadhavdigambar5371@gmail.com>
2024-10-06 15:12:46 +0530

    Test

--- src/Sept/Task_23092024/Task_23092024_016.java
+++ src/Sept/Task_23092024/Task_23092024_016.java
@@ -1,7 +1,6 @@
 package Sept.Task_23092024;
 
 import java.util.Scanner;
-
 public class Task_23092024_016 {
     public static void main(String[] args) {
         int num1, num2;
@@ -9,10 +8,30 @@ public class Task_23092024_016 {
         Scanner sc = new Scanner(System.in);
         num1 = sc.nextInt();
         num2 = sc.nextInt();
-        System.out.println(num1+num2);
-        System.out.println(num1-num2);
-        System.out.println(num1*num2);
-        System.out.println(num1%num2);
+        System.out.println("Enter the Operator:");
+        char op=sc.next().charAt(0);
+        int res=0;
+        switch (op) {
+            case '+':
+                System.out.println(num1+num2);
+                break;
+            case '-':
+                System.out.println(num1-num2);
+                break;
+            case '*':
+                System.out.println(num1*num2);
+                break;
+            case '/':
+                System.out.println(num1/num2);
+                break;
+            case '%':
+                System.out.println(num1%num2);
+                break;
+            default:
+                System.out.println("Invalid Operator! Please enter correct operator");
+                break;
 
+
+        }
     }
 }
---
+++ src/Sept/Task_23092024/Task_23092024_017.java
@@ -0,0 +1,7 @@
+package Sept.Task_23092024;
+
+public class Task_23092024_017 {
+    public static void main(String[] args) {
+
+    }
+}
