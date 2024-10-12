13cb1e40bf0085791e4319ac9e69d6ea4f84abc8
Digambar_Jadhav <jadhavdigambar5371@gmail.com>
2024-10-12 13:50:19 +0530

    Test

---
+++ src/Oct/ex_02102024/ATBStudent.java
@@ -0,0 +1,30 @@
+package Oct.ex_02102024;
+
+public class ATBStudent extends Object {
+    public static void main(String[] args) {
+
+
+    // C - ATBStudent
+    //  A - Attribute | data members | instance variables | fields | member variables
+    long phone;
+    String email;
+    int age;
+    int batch8x;
+    String name;
+    boolean isMale;
+    String address;
+    boolean isfeesPaid;
+    String githublink;
+    String sid;
+
+    //  B - Behaviour | methods | member functions
+    void study(){
+
+        } // NRNA
+    void eat(){
+
+        }
+    void walk(){
+
+        }
+}}
--- src/Oct/ex_02102024/Lab125.java
+++
@@ -1,7 +0,0 @@
-package Oct.ex_02102024;
-
-public class Lab125 {
-    public static void main(String[] args) {
-
-    }
-}
