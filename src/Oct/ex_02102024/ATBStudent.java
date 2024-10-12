e03f524e9b20f189eba41075c10f7c1775919afa
Digambar_Jadhav <jadhavdigambar5371@gmail.com>
2024-10-12 14:06:21 +0530

    Test

--- src/Oct/ex_02102024/ATBStudent.java
+++ src/Oct/ex_02102024/ATBStudent.java
@@ -1,9 +1,6 @@
 package Oct.ex_02102024;
 
 public class ATBStudent extends Object {
-    public static void main(String[] args) {
-
-
     // C - ATBStudent
     //  A - Attribute | data members | instance variables | fields | member variables
     long phone;
@@ -13,18 +10,12 @@ public class ATBStudent extends Object {
     String name;
     boolean isMale;
     String address;
-    boolean isfeesPaid;
+    boolean isfeedPaid;
     String githublink;
     String sid;
 
     //  B - Behaviour | methods | member functions
-    void study(){
-
-        } // NRNA
-    void eat(){
-
-        }
-    void walk(){
-
-        }
-}}
+    void study(){} // NRNA
+    void eat(){}
+    void walk(){}
+}
