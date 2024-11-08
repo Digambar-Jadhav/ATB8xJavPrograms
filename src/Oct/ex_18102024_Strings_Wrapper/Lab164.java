873fbacc5e58cadb151b4b5f9961ab37d3aec775
digambarjadhav <jadhavdigambar5371@gmail.com>
2024-11-08 18:58:01 +0530

    Test

---
+++ src/Oct/ex_18102024_Strings_Wrapper/Lab164.java
@@ -0,0 +1,10 @@
+package Oct.ex_18102024_Strings_Wrapper;
+
+public class Lab164 {
+    public static void main(String[] args) {
+        String S1 = "Hello";
+        S1.concat(" World");
+        System.out.println(S1);
+
+    }
+}
