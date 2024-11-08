961398ca5f48d776e9a097cd5e906df274fba89e
digambarjadhav <jadhavdigambar5371@gmail.com>
2024-11-08 18:53:25 +0530

    Test

---
+++ src/Oct/ex_18102024_Strings_Wrapper/Lab159.java
@@ -0,0 +1,16 @@
+package Oct.ex_18102024_Strings_Wrapper;
+
+public class Lab159 {
+    public static void main(String[] args) {
+        char c = 'A';
+        String name = "Digambar";
+        name = "Anvi";
+        name = "Anita";
+
+        String S1 = new String("Digambar");
+
+
+        String S2 = new String("Jadhav");
+
+    }
+}
---
+++ src/Oct/ex_18102024_Strings_Wrapper/Lab160_Equals.java
@@ -0,0 +1,16 @@
+package Oct.ex_18102024_Strings_Wrapper;
+
+public class Lab160_Equals {
+    public static void main(String[] args) {
+        String s1 = "Hello";
+        String s2 = "Hello";
+        String s3 = "Digambar";
+        String s4 = "Anita";
+        String s5 = "Anita";
+
+        System.out.println(s1.equals(s2));
+        System.out.println(s2.equals(s3));
+        System.out.println(s3.equals(s4));
+        System.out.println(s4.equals(s5));
+    }
+}
