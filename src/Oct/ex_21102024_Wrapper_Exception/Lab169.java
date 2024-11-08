7ca40bf0ce127ee40a7666c33f6ef8b0dd826950
digambarjadhav <jadhavdigambar5371@gmail.com>
2024-11-08 19:05:08 +0530

    Test

--- src/Oct/ex_21102024_Wrapper_Exception/Lab169.java
+++ src/Oct/ex_21102024_Wrapper_Exception/Lab169.java
@@ -3,8 +3,11 @@ package Oct.ex_21102024_Wrapper_Exception;
 public class Lab169 {
     public static void main(String[] args) {
         Integer a = 10;
-        String name = "PRamod";
-        Long phone = 9876543210L;
+        String name = "Digambar";
+        Long phone = 8378024376L;
+        System.out.println(name);
+        System.out.println(phone);
+
 
     }
 }
