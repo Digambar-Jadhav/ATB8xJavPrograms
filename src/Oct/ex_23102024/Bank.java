bd4096ddc72727effed597c967733cd109faa77e
digambarjadhav <jadhavdigambar5371@gmail.com>
2024-11-08 19:23:54 +0530

    Test

---
+++ src/Oct/ex_23102024/Bank.java
@@ -0,0 +1,40 @@
+package Oct.ex_23102024;
+import java.io.FileNotFoundException;
+public class Bank {
+    private String currency;
+    private Integer amount;
+
+    public Bank(String currency, Integer amount) {
+        this.currency = currency;
+        this.amount = amount;
+
+
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    public Integer getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Integer amount) {
+        this.amount = amount;
+    }
+
+    public Integer add(Bank bankName) throws FileNotFoundException, Exception {
+
+        if (!bankName.currency.equalsIgnoreCase("INR")) {
+//            throw new Exception("Currency Mismatch, Can't Proceed!");
+//            throw new Exception("Currency Mismatch, Can't Proceed!");
+            throw new Error();
+        }
+        return bankName.amount + this.amount;
+
+    }
+}
