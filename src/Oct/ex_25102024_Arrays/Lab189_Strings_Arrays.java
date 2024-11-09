2cfb45114341210420d10cdff499abbb813d14cc
digambarjadhav <jadhavdigambar5371@gmail.com>
2024-11-09 18:21:36 +0530

    Test

---
+++ src/Oct/ex_25102024_Arrays/Lab189_Strings_Arrays.java
@@ -0,0 +1,20 @@
+package Oct.ex_25102024_Arrays;
+
+public class Lab189_Strings_Arrays {
+    public static void main(String[] args) {
+        String [] Names = new String [5];
+        Names[0] = "Digambar";
+        Names[1] = "Jadhav";
+        Names[2] = "Anita";
+        Names[3] = "Anvi";
+        Names[4] = "Shri Gurudev Datta";
+        System.out.println(Names[0]);
+        System.out.println(Names[1]);
+        System.out.println(Names[2]);
+        System.out.println(Names[3]);
+        System.out.println(Names[4]);
+        //System.out.println(Names[5]);
+
+
+    }
+}
