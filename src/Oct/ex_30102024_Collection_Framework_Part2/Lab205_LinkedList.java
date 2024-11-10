4f41095fd3a0e3491c38814b591e5af916ec5517
digambarjadhav <jadhavdigambar5371@gmail.com>
2024-11-10 20:20:51 +0530

    Test

---
+++ src/Oct/ex_30102024_Collection_Framework_Part2/Lab205_LinkedList.java
@@ -0,0 +1,43 @@
+package Oct.ex_30102024_Collection_Framework_Part2;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Vector;
+
+public class Lab205_LinkedList {
+    public static void main(String[] args) {
+        // LinkedList mylist = new LinkedList();
+        List mylist = new LinkedList();
+        mylist.add(1);
+        mylist.add(2);
+        mylist.add(3);
+        mylist.add(4);
+        mylist.add(4);
+        System.out.println(mylist);
+
+        Integer[] a = new Integer[5];
+        String[] names = new String[5];
+
+
+        List mylist1 = new ArrayList(); // FR - CO
+        mylist1.add("Digambar");
+        mylist1.add("Anvi");
+        mylist1.add("Anita");
+        System.out.println(mylist1);
+
+
+        //  Selenium -  -> Store and Insertion and Deletetion - DS - LL
+        // api - STORE, access -> AL
+
+
+
+
+
+
+
+
+
+
+    }
+}
