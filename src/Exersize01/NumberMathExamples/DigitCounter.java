ed85225d9b980a6042e29a67d45736bc5a8a1e82
Your Name <jadhavdigambar5371@gmail.com>
2025-04-25 20:38:15 +0530

    Testing

---
+++ src/Exersize01/NumberMathExamples/DigitCounter.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class DigitCounter {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        long number = scanner.nextLong();
+
+        int digitCount = countDigits(number);
+
+        System.out.println("Number of digits: " + digitCount);
+    }
+
+    public static int countDigits(long num) {
+        // Handle negative numbers
+        if (num < 0) {
+            num = -num;
+        }
+
+        // Handle case where number is 0
+        if (num == 0) {
+            return 1;
+        }
+
+        int count = 0;
+        while (num != 0) {
+            num /= 10;
+            count++;
+        }
+        return count;
+    }
+}
